1dd2e9ef7f339adaffc929c456cc3cccdaaf0879
Koen Verburg <creativekoen@gmail.com>
2018-07-13 14:11:06 +0200

    create powershell script to download plug

---
+++ neovim/installPlug.ps1
@@ -0,0 +1,8 @@
+mkdir ~\AppData\Local\nvim\autoload
+$uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+(New-Object Net.WebClient).DownloadFile(
+  $uri,
+  $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
+    "~\AppData\Local\nvim\autoload\plug.vim"
+  )
+)
