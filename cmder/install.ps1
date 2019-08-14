77c0707021b8ee53fb18c80048e129441c5865aa
unknown <creativekoen@gmail.com>
2019-08-14 21:23:55 +0200

    refactor: test if the file exists before symlinking

--- cmder/install.ps1
+++ cmder/install.ps1
@@ -1,9 +1,36 @@
-New-Item -ItemType SymbolicLink -Path "C:\Users\$($env:USERNAME)\Documents\WindowsPowerShell\poshThemes\koenverburg.psm1" -Value ".\cmder\koenverburg.psm1" -Force >> $null
-New-Item -ItemType SymbolicLink -Path "C:\Users\$($env:USERNAME)\Documents\PowerShell\poshThemes\koenverburg.psm1" -Value ".\cmder\koenverburg.psm1" -Force >> $null
+$koenverburgThemepwsh5 = "C:\Users\$($env:USERNAME)\Documents\WindowsPowerShell\poshThemes\koenverburg.psm1"
+$koenverburgThemepwsh6 = "C:\Users\$($env:USERNAME)\Documents\PowerShell\poshThemes\koenverburg.psm1"
 
-New-Item -ItemType SymbolicLink -Path "C:\Users\$($env:USERNAME)\Documents\WindowsPowerShell\poshThemes\pure_koenverburg.psm1" -Value ".\cmder\pure_koenverburg.psm1" -Force >> $null
-New-Item -ItemType SymbolicLink -Path "C:\Users\$($env:USERNAME)\Documents\PowerShell\poshThemes\pure_koenverburg.psm1" -Value ".\cmder\pure_koenverburg.psm1" -Force >> $null
+$kvPure_pwsh5 = "C:\Users\$($env:USERNAME)\Documents\WindowsPowerShell\poshThemes\pure_koenverburg.psm1"
+$kvPure_pwsh6 = "C:\Users\$($env:USERNAME)\Documents\PowerShell\poshThemes\pure_koenverburg.psm1"
+
+if (-Not (Test-Path -Path $koenverburgThemepwsh5)) {
+  New-Item -ItemType SymbolicLink -Path $koenverburgThemepwsh5 -Value ".\cmder\koenverburg.psm1" -Force >> $null
+}
+Else { Write-Information "It's is there" }
+
+if (-Not (Test-Path -Path $koenverburgThemepwsh6)) {
+  New-Item -ItemType SymbolicLink -Path $koenverburgThemepwsh6 -Value ".\cmder\koenverburg.psm1" -Force >> $null
+}
+Else { Write-Information "It's is there" }
+
+if (-Not (Test-Path -Path $kvPure_pwsh5)) {
+  New-Item -ItemType SymbolicLink -Path $kvPure_pwsh5 -Value ".\cmder\pure_koenverburg.psm1" -Force >> $null
+}
+Else { Write-Information "It's is there" }
+
+if (-Not (Test-Path -Path $kvPure_pwsh6)) {
+  New-Item -ItemType SymbolicLink -Path $kvPure_pwsh6 -Value ".\cmder\pure_koenverburg.psm1" -Force >> $null
+}
+Else { Write-Information "It's is there" }
+
 Write-Output "[i] Create a a symlink for the cmder theme"
 
-.\ConEmu-Color-Themes\Install-ConEmuTheme.ps1 -ConfigPath "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml" -Operation Add -ThemePathOrName ".\ConEmu-Color-Themes\themes\ayu Mirage.xml"
+$cmderConfig = "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml"
+
+if (-Not (Test-Path -Path $cmderConfig)) {
+  .\ConEmu-Color-Themes\Install-ConEmuTheme.ps1 -ConfigPath -Operation Add -ThemePathOrName ".\ConEmu-Color-Themes\themes\ayu Mirage.xml"
+}
+Else { Write-Information "It's is there" }
+
 Write-Output "[i] Install the ayu theme"
