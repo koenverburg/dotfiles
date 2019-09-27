56791a559b3baf98e1481b8921b51f4713d69c2a
unknown <creativekoen@gmail.com>
2019-09-27 18:12:06 +0200

    chore: opted for normal firefox and non lts node
    Also commented out c?make ctaga and mingw because I probably dont need them because of cleaning up my neovim config

--- bin/programs.ps1
+++ bin/programs.ps1
@@ -3,14 +3,16 @@ Install 7zip
 Install curl
 Install greenshot
 Install filezilla
-Install firefox-dev $true
+Install firefox
 Install Ruby
+Install nodejs
 Install python3
-Install mingw
-Install ctags
-Install make
-Install cmake
+# Install mingw
+# Install ctags
+# Install make
+# Install cmake
 Install neovim
 Install cmder
-Install nodejs-lts
 Install yarn $true
+Install powershell-core
+Install vscode
