01c1f2600f8a622b219653ab2bbf478c83f6726a
unknown <creativekoen@gmail.com>
2019-09-27 18:10:39 +0200

    feat(devtools script) added cmder config and neovim config

--- bin/devtools.ps1
+++ bin/devtools.ps1
@@ -1,24 +1,24 @@
-# # PowerShell Modules
-# # this will run under powershell 5
-# if (!(Get-Module posh-git)) {
-#   Install-Module posh-git -Force
-# }
+# PowerShell Modules
+this will run under powershell 5
+if (!(Get-Module posh-git)) {
+  Install-Module posh-git -Force
+}
 
-# if (!(Get-Module oh-my-posh)) {
-#   Install-Module oh-my-posh -Force
-# }
+if (!(Get-Module oh-my-posh)) {
+  Install-Module oh-my-posh -Force
+}
 
-# if (!(Get-Module Get-ChildItemColor)) {
-#   Install-Module -Name Get-ChildItemColor -Force
-# }
+if (!(Get-Module Get-ChildItemColor)) {
+  Install-Module -Name Get-ChildItemColor -Force
+}
 
-# # this will be installed under pwsh 6 core
-# if (Get-Command 'pwsh.exe') {
-#   start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module posh-git -Force'
-#   start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module oh-my-posh -Force'
-#   start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module Get-ChildItemColor -Force -AllowClobber'
-#   start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module -Name PSReadLine -AllowPrerelease -Force -SkipPublisherCheck'
-# }
+# this will be installed under pwsh 6 core
+if (Get-Command 'pwsh.exe') {
+  start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module posh-git -Force'
+  start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module oh-my-posh -Force'
+  start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module Get-ChildItemColor -Force -AllowClobber'
+  start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module -Name PSReadLine -AllowPrerelease -Force -SkipPublisherCheck'
+}
 
 # Powershell Profile(s)
 StowFile $env:HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 (Get-Item ".\powershell\profile.ps1").FullName
@@ -34,18 +34,21 @@ if ($terminalFolder) {
   StowFile "$env:LOCALAPPDATA\$cleanTerminalPath\RoamingState\profiles.json" (Get-Item ".\Terminal\profiles.json").FullName
 }
 
-# # vs Code
-# Install vscode
+# vs Code
 StowFile $env:APPDATA\Code\User\settings.json (Get-Item ".\vscode\settings.json").FullName
 StowFile $env:APPDATA\Code\User\keybindings.json (Get-Item ".\vscode\keybindings.json").FullName
 
-# # vs code Insiders
+# vs code Insiders Dont know if i'm keeping vscode insiders since my neovim is working again
 # Install visualstudiocode-insiders $true
-StowFile "$env:APPDATA\Code - Insiders\User\settings.json" (Get-Item ".\vscode\settings.json").FullName
-StowFile "$env:APPDATA\Code - Insiders\User\keybindings.json" (Get-Item ".\vscode\keybindings.json").FullName
+# StowFile "$env:APPDATA\Code - Insiders\User\settings.json" (Get-Item ".\vscode\settings.json").FullName
+# StowFile "$env:APPDATA\Code - Insiders\User\keybindings.json" (Get-Item ".\vscode\keybindings.json").FullName
 
 # Cmder config
-# .\ConEmu-Color-Themes\Install-ConEmuTheme.ps1 -ConfigPath -Operation Add -ThemePathOrName ".\ConEmu-Color-Themes\themes\ayu Mirage.xml"
+if (Test-Path "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml") {
+  Remove-Item "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml" -Force
+  StowFile "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml" (Get-Item ".\cmder\setttings.xml").FullName
+}
+# .\ConEmu-Color-Themes\Install-ConEmuTheme.ps1 -ConfigPath "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml" -Operation Add -ThemePathOrName ".\ConEmu-Color-Themes\themes\ayu Mirage.xml"
 
 # oh my posh theme
 StowFile "$env:HOME\Documents\WindowsPowerShell\poshThemes\koenverburg.psm1" (Get-Item ".\cmder\koenverburg.psm1").FullName
@@ -54,5 +57,17 @@ StowFile "$env:HOME\Documents\WindowsPowerShell\poshThemes\pure_koenverburg.psm1
 StowFile "$env:HOME\Documents\Powershell\poshThemes\pure_koenverburg.psm1" (Get-Item ".\cmder\pure_koenverburg.psm1").FullName
 
 # Neovim
+if (-not (Test-Path "C:\Users\$($env:USERNAME)\AppData\Local\nvim\autoload")) {
+  mkdir -Path "C:\Users\$($env:USERNAME)\AppData\Local\nvim\autoload" >> $null
+
+  $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  (New-Object Net.WebClient).DownloadFile(
+    $uri,
+    $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
+      "C:\Users\$($env:USERNAME)\AppData\Local\nvim\autoload\plug.vim"
+    )
+  )
+}
+
 StowFile $env:LOCALAPPDATA\nvim\ginit.vim (Get-Item ".\neovim\ginit.vim").FullName
 StowFile $env:LOCALAPPDATA\nvim\init.vim (Get-Item ".\neovim\init.vim").FullName
