2887566773fa68abbe5d70bfa54a0551ac01e467
Koen Verburg <creativekoen@gmail.com>
2020-03-20 23:25:34 +0100

    feat(macos): first stabs on a bootstrapper for macos

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 # install xcode
 
-# echo "Installing homebrew ..."
+echo "Let's get this Arrow Flying.."
 
 which -s brew
 if [[ $? != 0 ]] ; then
@@ -11,7 +11,6 @@ fi
 
 brew update
 
-# brew install yarn, vscode, firefox, chrome, slack, insomnia, Iterm2, Alla
 # Install zsh
 brew install zsh
 brew install zplug
@@ -23,35 +22,74 @@ brew tap caskroom/versions
 brew cask install firefox
 brew cask install google-chrome
 
-# Other stuff
+Other stuff
 brew cask install spotify
 brew cask install alacritty
 
+# Maybe, maybe not
 # brew cask install alfred
 # brew cask install bartender
 # brew cask install karabiner-elements
 # brew cask install flux
 
 # Development stuff
+brew install go
 brew install watchman
 brew install tmux
 brew install git
-brew instal htop
+brew install node
+brew install yarn
+brew install rbenv
+rbenv init
+rbenv install 2.7.0
+rbenv shell 2.7.0
+npm install -g artillery serve
+# Search
+brew install the_silver_searcher
+brew install rg
+brew install ack # find in files
+brew install fzf # fuzzy finder
 
 # neovim
-brew install python3
+brew install python3  # brew install is not overriding the build in one...
 brew install neovim
 pip3 install neovim
 pip3 install pynvim
+gem install neovim
+sudo rm -R ~/.local # removing stuff so that nvim can startup in one go
+
+# Install other useful binaries.
+brew install htop
+# brew install youtube-dl
+
+# Fonts
+https://github.com/caskroom/homebrew-fonts
+brew tap homebrew/cask-fonts
+brew cask install homebrew/cask-fonts/font-hack
+brew cask install homebrew/cask-fonts/font-hack-nerd-font
+brew cask install homebrew/cask-fonts/font-source-code-pro
 
 brew cask cleanup
 
+gem update --system
+gem install tmuxinator
+gem install foreman
 
+# neovim setups (partly)
+# Neovim
+if [ -e "$HOME/.config/nvim/autoload/plug.vim" ]; then
+  echo "vim plug exists for neovim, skipping ..."
+else
+  curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
-# install the fonts
 # symlink all the dotfiles
 
-ln -s /Users/koenverburg/code/github/dotfiles/vscode/settings.json /Users/koenverburg/Library/Application\ Support/Code/User/settings.json
-ln -s /Users/koenverburg/code/github/dotfiles/vscode/keybindings.json /Users/koenverburg/Library/Application\ Support/Code/User/keybindings.json
+# Terminal setup
+ln -s ~/code/github/dotfiles/tmuxinator/ ~/.config/tmuxinator/
+ln -s ~/code/github/dotfiles/nvim/ ~/.config/nvim/
 
+# VS code
+ln -s ˜/code/github/dotfiles/vscode/settings.json ˜/Library/Application\ Support/Code/User/settings.json
+ln -s ˜/code/github/dotfiles/vscode/keybindings.json ˜/Library/Application\ Support/Code/User/keybindings.json
 while read line; do code --install-extension $line; done < vscode/extensions.txt
