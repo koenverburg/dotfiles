0511345676bac24f2feb297f471cb6818faeb547
Koen Verburg <creativekoen@gmail.com>
2020-04-30 22:15:17 +0200

    chore: marked cmder as uninstall

--- bin/programs.ps1
+++ bin/programs.ps1
@@ -6,12 +6,12 @@ Install elixir #includes erlang,mix
 Install python3
 Install make
 Install neovim
-Install cmder
 Install yarn
 Install vscode
 Install powershell-core
 Install alacritty
 
+Uninstall cmder
 # Install 7zip
 # Install filezilla
 # Install curl
