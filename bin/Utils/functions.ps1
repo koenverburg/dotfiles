99e6afc83382b6fe415deb6d308ece2e22e9d29b
Koen Verburg <creativekoen@gmail.com>
2020-04-30 22:14:51 +0200

    improve: added a uninstall method to uninstall programs

--- bin/Utils/functions.ps1
+++ bin/Utils/functions.ps1
@@ -71,3 +71,13 @@ function Install([String]$package, [bool]$beta = $false, [bool]$skipCheckSum = $
     Write-Output "[i] Package $package already installed"
   }
 }
+
+function Uninstall([String]$package) {
+  if (((choco list $package --exact --local-only --limitoutput) -like "$package*")) {
+    Write-Output "[i] package $package is installed, removing it.."
+    choco uninstall $package -y
+  }
+  else {
+    Write-Output "[i] Package $package is already removed, consider removing this uninstall from your bootstrapper"
+  }
+}
