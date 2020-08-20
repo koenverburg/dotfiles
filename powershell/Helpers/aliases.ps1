86f3baf725722fa60e5a6c6c7fa74ae8b5252a3a
Koen Verburg <creativekoen@gmail.com>
2020-08-20 18:48:05 +0200

    chore(aliases): added new aliases

--- powershell/Helpers/aliases.ps1
+++ powershell/Helpers/aliases.ps1
@@ -1,6 +1,5 @@
 Import-Module "$home\code\github\dotfiles\powershell\Helpers\Utils.ps1"
 
-function gf { git fetch --prune $args }
 function gco { git checkout $args }
 function gcob { git checkout -b $args }
 function gcod { git checkout development $args }
@@ -8,7 +7,9 @@ function gcom { git checkout master $args }
 function gs { git status -sb $args }
 function gaa { git add --all $args }
 function gl { git log $args }
-function glo { git log --oneline $args }
+function glo {
+  Write-Output "use plog"
+}
 
 function gpo {
   $CurrentBranch = Get-Git-CurrentBranch
@@ -43,13 +44,24 @@ function cpanel {
   wt -p $wtProfile -d $args `; split-pane -V -p $wtProfile -d $args `; split-pane -H -p $wtProfile -d $args
 }
 
-# folder jumping
+# Folder Jumping
 function dot {
   Set-Location "~/code/github/dotfiles"
 }
 
-# yarn aliases
-function ys { yarn serve }
-function yt { yarn test }
-function yd { yarn dist }
-function y { yarn }
+# Yarn aliases
+New-Alias y 'yarn' -Scope Global
+New-Alias yw 'yarn watch' -Scope Global
+New-Alias ys 'yarn serve' -Scope Global
+New-Alias yt 'yarn test' -Scope Global
+New-Alias yd 'yarn dist' -Scope Global
+New-Alias yb 'yarn build' -Scope Global
+New-Alias yupgrade 'yarn upgrade-interactive --latest' -Scope Global
+
+# Git Aliases (late 2020 refactor)
+
+New-Alias gf 'git fetch --prune' -Scope Global -PassThru
+New-Alias gb 'git branch' -Scope Global
+New-Alias unmerged "git branch --no-merged" -Scope Global
+New-Alias plog "git log --oneline --decorate" -Scope Global
+New-Alias s 'git status -sb' -Scope Global -PassThru
