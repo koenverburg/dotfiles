c1689a6b023240475dc7fc794e4f179d486dc6b9
Koen Verburg <creativekoen@gmail.com>
2020-10-20 14:10:13 +0200

    feat(aliases): added a new projects func

--- powershell/Aliases/folderjumping.ps1
+++ powershell/Aliases/folderjumping.ps1
@@ -2,3 +2,10 @@
 function dot {
   Set-Location "~/code/github/dotfiles"
 }
+
+function projects {
+  $firstTab = '--title "Dotfiles" -p "Powershell" -d "$HOME\code\github\dotfiles"'
+  $secondTab = '--title "site" -p "Powershell" -d "$HOME\code\github\koenverburg.dev"'
+  $thirdTab = '--title "Github readme" -p "Powershell" -d "$HOME\code\github\koenverburg"'
+  Start-Process wt "$firstTab `; $secondTab `; $thirdTab";
+}
