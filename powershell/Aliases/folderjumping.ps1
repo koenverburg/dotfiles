ffa424c627206f33bec26e28177a318bfc2812e3
Koen Verburg <creativekoen@gmail.com>
2020-11-03 22:19:50 +0100

    chore(pwsh): commented out an function that is not finished

--- powershell/Aliases/folderjumping.ps1
+++ powershell/Aliases/folderjumping.ps1
@@ -3,19 +3,19 @@ function dot {
   Set-Location "~/code/github/dotfiles"
 }
 
-function projects {
-  $dotsTitle = "Dotfiles"
-  $dotsPath = $(Resolve-Path ~\code\github\dotfiles).Path
+# function projects {
+#   $dotsTitle = "Dotfiles"
+#   $dotsPath = $(Resolve-Path ~\code\github\dotfiles).Path
 
-  $obsidianTitle = "obsidian"
-  $obsidianPath = $(Resolve-Path ~\code\github\dotfiles).Path
+#   $obsidianTitle = "obsidian"
+#   $obsidianPath = $(Resolve-Path ~\code\github\dotfiles).Path
 
-  $tabs = "--title="$($dotsTitle)" -d="$($dotsPath)""
+#   $tabs = "--title="$($dotsTitle)" -d="$($dotsPath)""
 
-    # "--title=""$obsidianTitle"" -d=""$obsidianPath""";
+#     # "--title=""$obsidianTitle"" -d=""$obsidianPath""";
 
-  wt.exe "$($tabs -Join " ``; ")"
-    # --title="site" -d=$(Resolve-Path ~\code\github\koenverburg.dev).Path `;
-    # --title="obsidian" -d=$(Resolve-Path ~\code\github\obsidian).Path `;
-    # --title="Readme" -d=$(Resolve-Path ~\code\github\koenverburg).Path `;
-}
+#   wt.exe "$($tabs -Join " ``; ")"
+#     # --title="site" -d=$(Resolve-Path ~\code\github\koenverburg.dev).Path `;
+#     # --title="obsidian" -d=$(Resolve-Path ~\code\github\obsidian).Path `;
+#     # --title="Readme" -d=$(Resolve-Path ~\code\github\koenverburg).Path `;
+# }
