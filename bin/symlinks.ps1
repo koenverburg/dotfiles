a05bb217e7fc3b23377111e658f45b11bb54e9dc
Koen Verburg <creativekoen@gmail.com>
2020-12-17 17:35:54 +0100

    core(Bootstrapper): updated symlinks

--- bin/symlinks.ps1
+++ bin/symlinks.ps1
@@ -1,5 +1,5 @@
 # PowerShell Modules
-# this will run under powershell 5
+# this will run under powershell 5, this should be placed in another file.
 if (!(Get-Module posh-git)) {
   Install-Module posh-git -Force
 }
@@ -22,10 +22,12 @@ if (Get-Command 'pwsh.exe') {
 }
 
 # Powershell Profile(s)
-StowFile $env:HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 (Get-Item ".\powershell\profile.ps1").FullName
-# StowFile $env:HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 (Get-Item ".\powershell\profile.ps1").FullName # powershell 5, no need for it
 
-# Install microsoft-windows-terminal, Always delete and place a new one
+StowFile $env:HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 (Get-Item ".\powershell\profile.ps1").FullName
+## StowFile $env:HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 (Get-Item ".\powershell\profile.ps1").FullName # powershell 5, no need for it
+
+## Install microsoft-windows-terminal, Always delete and place a new one
+# TODO place this in another file.
 $terminalFolder = Get-ChildItem "C:\Users\$($env:USERNAME)\AppData\Local\Packages" -filter "Microsoft.WindowsTerminal_*" -Directory | ForEach-Object { $_.fullname }
 if ($terminalFolder) {
   $cleanTerminalPath = $terminalFolder.Substring("C:\Users\$env:USERNAME\AppData\Local\".Length)
@@ -35,31 +37,16 @@ if ($terminalFolder) {
   StowFile "$env:LOCALAPPDATA\$cleanTerminalPath\LocalState\settings.json" (Get-Item ".\Terminal\settings.json").FullName
 }
 
-# vs Code
-StowFile $env:APPDATA\Code\User\settings.json (Get-Item ".\vscode\settings.json").FullName
-StowFile $env:APPDATA\Code\User\keybindings.json (Get-Item ".\vscode\keybindings.json").FullName
-
-# Neovim
-# installing Plug the plugin manager for Vim
-if (-not (Test-Path "C:\Users\$($env:USERNAME)\AppData\Local\nvim\autoload")) {
-  mkdir -Path "C:\Users\$($env:USERNAME)\AppData\Local\nvim\autoload" >> $null
-
-  $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-  (New-Object Net.WebClient).DownloadFile(
-    $uri,
-    $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
-      "C:\Users\$($env:USERNAME)\AppData\Local\nvim\autoload\plug.vim"
-    )
-  )
-} else {
-  Write-Output "[!] Neovim plug is already installed"
-}
+## Neovim
 
 # symlinking the config
 StowFile $env:LOCALAPPDATA\nvim\init.vim (Get-Item ".\nvim\init.vim").FullName
 StowFile $env:LOCALAPPDATA\nvim\ginit.vim (Get-Item ".\nvim\ginit.vim").FullName
 
 # All the other files
+Stow ".\nvim\lua" "$env:LOCALAPPDATA\nvim\lua"
+Stow ".\nvim\lua\kv" "$env:LOCALAPPDATA\nvim\lua\kv"
+Stow ".\nvim\lua\finder" "$env:LOCALAPPDATA\nvim\lua\finder"
 Stow ".\nvim\core" "$env:LOCALAPPDATA\nvim\core"
 Stow ".\nvim\config" "$env:LOCALAPPDATA\nvim\config"
 Stow ".\nvim\plugins" "$env:LOCALAPPDATA\nvim\plugins"
@@ -67,3 +54,7 @@ Stow ".\nvim\settings" "$env:LOCALAPPDATA\nvim\settings"
 
 # Alacritty
 StowFile $env:APPDATA\alacritty\alacritty.yml (Get-Item ".\alacritty\alacritty.yml").FullName
+
+# vs Code
+StowFile $env:APPDATA\Code\User\settings.json (Get-Item ".\vscode\settings.json").FullName
+StowFile $env:APPDATA\Code\User\keybindings.json (Get-Item ".\vscode\keybindings.json").FullName
