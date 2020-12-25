8f53c083e348eba6dd7fa5596d1b9e3f0359cce2
Koen Verburg <creativekoen@gmail.com>
2020-12-25 13:58:06 +0100

    refactor: updated affected paths

--- bin/symlinks.ps1
+++ bin/symlinks.ps1
@@ -1,32 +1,27 @@
 # PowerShell Modules
 # this will run under powershell 5, this should be placed in another file.
-if (!(Get-Module posh-git)) {
-  Install-Module posh-git -Force
-}
+# if (!(Get-Module posh-git)) {
+#   Install-Module posh-git -Force
+# }
 
-if (!(Get-Module oh-my-posh)) {
-  Install-Module oh-my-posh -Force
-}
+# if (!(Get-Module oh-my-posh)) {
+#   Install-Module oh-my-posh -Force
+# }
 
-if (!(Get-Module Get-ChildItemColor)) {
-  Install-Module -Name Get-ChildItemColor -Force
-}
+# if (!(Get-Module Get-ChildItemColor)) {
+#   Install-Module -Name Get-ChildItemColor -Force
+# }
 
-# this will be installed under pwsh 6 core
-if (Get-Command 'pwsh.exe') {
-  start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module windows-screenfetch -Force'
-  start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module posh-git -Force'
-  start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module oh-my-posh -Force'
-  start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module Get-ChildItemColor -Force -AllowClobber'
-  start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module -Name PSReadLine -AllowPrerelease -Force -SkipPublisherCheck'
-}
+# # this will be installed under pwsh 6 core
+# if (Get-Command 'pwsh.exe') {
+#   start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module windows-screenfetch -Force'
+#   start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module posh-git -Force'
+#   start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module oh-my-posh -Force'
+#   start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module Get-ChildItemColor -Force -AllowClobber'
+#   start-process pwsh.exe -argument '-nologo -noprofile -command Install-Module -Name PSReadLine -AllowPrerelease -Force -SkipPublisherCheck'
+# }
 
-# Powershell Profile(s)
-
-StowFile $env:HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 (Get-Item ".\powershell\profile.ps1").FullName
-## StowFile $env:HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 (Get-Item ".\powershell\profile.ps1").FullName # powershell 5, no need for it
-
-## Install microsoft-windows-terminal, Always delete and place a new one
+# Install microsoft-windows-terminal, Always delete and place a new one
 # TODO place this in another file.
 $terminalFolder = Get-ChildItem "C:\Users\$($env:USERNAME)\AppData\Local\Packages" -filter "Microsoft.WindowsTerminal_*" -Directory | ForEach-Object { $_.fullname }
 if ($terminalFolder) {
@@ -34,27 +29,31 @@ if ($terminalFolder) {
   if (Test-Path "$env:LOCALAPPDATA\$cleanTerminalPath\LocalState\settings.json") {
     Remove-Item "$env:LOCALAPPDATA\$cleanTerminalPath\LocalState\settings.json" -Force
   }
-  StowFile "$env:LOCALAPPDATA\$cleanTerminalPath\LocalState\settings.json" (Get-Item ".\Terminal\settings.json").FullName
+  StowFile "$env:LOCALAPPDATA\$cleanTerminalPath\LocalState\settings.json" (Get-Item ".\.config\Terminal\settings.json").FullName
 }
+
+# Powershell Profile(s)
+StowFile $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 (Get-Item ".\.config\powershell\profile.ps1").FullName
+StowFile $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 (Get-Item ".\.config\powershell\profile.ps1").FullName # powershell 5, no need for it
 
 ## Neovim
 
 # symlinking the config
-StowFile $env:LOCALAPPDATA\nvim\init.vim (Get-Item ".\nvim\init.vim").FullName
-StowFile $env:LOCALAPPDATA\nvim\ginit.vim (Get-Item ".\nvim\ginit.vim").FullName
+StowFile $env:LOCALAPPDATA\nvim\init.vim (Get-Item ".\.config\nvim\init.vim").FullName
+# StowFile $env:LOCALAPPDATA\nvim\ginit.vim (Get-Item ".\.config\nvim\ginit.vim").FullName
 
 # All the other files
-Stow ".\nvim\lua" "$env:LOCALAPPDATA\nvim\lua"
-Stow ".\nvim\lua\kv" "$env:LOCALAPPDATA\nvim\lua\kv"
-Stow ".\nvim\lua\finder" "$env:LOCALAPPDATA\nvim\lua\finder"
-Stow ".\nvim\core" "$env:LOCALAPPDATA\nvim\core"
-Stow ".\nvim\config" "$env:LOCALAPPDATA\nvim\config"
-Stow ".\nvim\plugins" "$env:LOCALAPPDATA\nvim\plugins"
-Stow ".\nvim\settings" "$env:LOCALAPPDATA\nvim\settings"
+Stow ".\.config\nvim\lua" "$env:LOCALAPPDATA\nvim\lua"
+Stow ".\.config\nvim\lua\kv" "$env:LOCALAPPDATA\nvim\lua\kv"
+Stow ".\.config\nvim\lua\finder" "$env:LOCALAPPDATA\nvim\lua\finder"
+Stow ".\.config\nvim\core" "$env:LOCALAPPDATA\nvim\core"
+Stow ".\.config\nvim\config" "$env:LOCALAPPDATA\nvim\config"
+Stow ".\.config\nvim\plugins" "$env:LOCALAPPDATA\nvim\plugins"
+Stow ".\.config\nvim\settings" "$env:LOCALAPPDATA\nvim\settings"
 
 # Alacritty
-StowFile $env:APPDATA\alacritty\alacritty.yml (Get-Item ".\alacritty\alacritty.yml").FullName
+StowFile $env:APPDATA\alacritty\alacritty.yml (Get-Item ".\.config\alacritty\alacritty.yml").FullName
 
 # vs Code
-StowFile $env:APPDATA\Code\User\settings.json (Get-Item ".\vscode\settings.json").FullName
-StowFile $env:APPDATA\Code\User\keybindings.json (Get-Item ".\vscode\keybindings.json").FullName
+StowFile $env:APPDATA\Code\User\settings.json (Get-Item ".\.config\vscode\settings.json").FullName
+StowFile $env:APPDATA\Code\User\keybindings.json (Get-Item ".\.config\vscode\keybindings.json").FullName
