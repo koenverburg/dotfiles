96cfe18e8337747b028788245fbbebf2586db60a
Koen Verburg <creativekoen@gmail.com>
2021-01-28 16:33:58 +0100

    chore(nvim): moved to packer.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,80 +1,103 @@
-local repo = 'https://github.com/savq/paq-nvim.git'
-local install_path = vim.fn.stdpath('data') .. '/site/pack/paq/opt/paq-nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  print 'Downloading Plugin Manager..'
-  vim.api.nvim_command(string.format('!git clone %s "%s"', repo, install_path))
+-- On ly required if you have packer in your `opt` pack
+local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
+
+if not packer_exists then
+  -- TODO: Maybe handle windows better?
+  if vim.fn.input("Download Packer? (y for yes)") ~= "y" then
+    return
+  end
+
+  local directory = string.format(
+    '%s/site/pack/packer/opt/',
+    vim.fn.stdpath('data')
+  )
+
+  vim.fn.mkdir(directory, 'p')
+
+  local out = vim.fn.system(string.format(
+    'git clone %s "%s"',
+    'https://github.com/wbthomason/packer.nvim',
+    directory .. '/packer.nvim'
+  ))
+
+  print(out)
+  print("Downloading packer.nvim...")
+  print("( You'll need to restart now )")
+
+  return
 end
 
-vim.cmd [[ packadd paq-nvim ]]
-local paq = require('paq-nvim').paq
+return require('packer').startup {
+  function(use)
+    -- use 'tweekmonster/startuptime.vim'
+    use {'wbthomason/packer.nvim', opt = true}
 
-paq 'savq/paq-nvim'
--- paq 'tweekmonster/startuptime.vim'
+    -- Colorscheme
+    use 'glepnir/zephyr-nvim'
+    use 'christianchiarulli/nvcode-color-schemes.vim'
 
--- Colorscheme
-paq 'glepnir/zephyr-nvim'
-paq 'christianchiarulli/nvcode-color-schemes.vim'
+    -- Finders
+    use 'nvim-lua/popup.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use 'nvim-telescope/telescope.nvim'
+    use 'junegunn/fzf'
+    use 'junegunn/fzf.vim'
 
--- Finders
-paq 'nvim-lua/popup.nvim'
-paq 'nvim-lua/plenary.nvim'
-paq 'nvim-telescope/telescope.nvim'
-paq 'junegunn/fzf'
-paq 'junegunn/fzf.vim'
+    -- LSP config
+    use 'neovim/nvim-lsp'
+    use 'neovim/nvim-lspconfig'
+    use 'nvim-lua/completion-nvim'
+    use 'nvim-lua/diagnostic-nvim'
 
--- LSP config
-paq 'neovim/nvim-lsp'
-paq 'neovim/nvim-lspconfig'
-paq 'nvim-lua/completion-nvim'
-paq 'nvim-lua/diagnostic-nvim'
+    -- Snippets
+    --use 'SirVer/ultisnips'
+    --use 'honza/vim-snippets'
 
--- Snippets
---paq 'SirVer/ultisnips'
---paq 'honza/vim-snippets'
+    use 'nvim-treesitter/nvim-treesitter'
 
-paq 'nvim-treesitter/nvim-treesitter'
+    -- Misc
+    use 'tpope/vim-surround' -- Able to change [{()}]""''
+    use 'RRethy/vim-illuminate'
 
--- Misc
-paq 'tpope/vim-surround' -- Able to change [{()}]""''
-paq 'RRethy/vim-illuminate'
+    -- Lanaguages
+    use 'elzr/vim-json'
+    use 'stephpy/vim-yaml'
+    use 'PProvost/vim-ps1'
 
--- Lanaguages
-paq 'elzr/vim-json'
-paq 'stephpy/vim-yaml'
-paq 'PProvost/vim-ps1'
+    --  use 'plasticboy/vim-markdown'
+    --  use 'godlygeek/tabular'
 
---  paq 'plasticboy/vim-markdown'
---  paq 'godlygeek/tabular'
+    -- WordJumping like resharper and faster movement
+    -- use 'bkad/CamelCaseMotion'
+    -- use 'easymotion/vim-easymotion'
 
--- WordJumping like resharper and faster movement
--- paq 'bkad/CamelCaseMotion'
--- paq 'easymotion/vim-easymotion'
+    -- Convenience for commenting things in and out
+    use 'scrooloose/nerdcommenter'
 
--- Convenience for commenting things in and out
-paq 'scrooloose/nerdcommenter'
+    -- Auto close parens, braces, brackets, etc
+    use 'jiangmiao/auto-pairs'
 
--- Auto close parens, braces, brackets, etc
-paq 'jiangmiao/auto-pairs'
+    -- Looks
+    use 'mhinz/vim-startify'
+    use 'ryanoasis/vim-devicons'
+    use 'kyazdani42/nvim-web-devicons'
+    use 'hoob3rt/lualine.nvim'
 
--- Looks
-paq 'mhinz/vim-startify'
-paq 'ryanoasis/vim-devicons'
-paq 'kyazdani42/nvim-web-devicons'
-paq 'hoob3rt/lualine.nvim'
+    -- Git feedback in files
+    -- use 'airblade/vim-gitgutter'
+    use 'mhinz/vim-signify'
+    use 'tpope/vim-fugitive'
 
--- Git feedback in files
--- paq 'airblade/vim-gitgutter'
-paq 'mhinz/vim-signify'
-paq 'tpope/vim-fugitive'
+    -- Focus mode
+    use 'junegunn/goyo.vim'
+    use 'junegunn/limelight.vim'
 
--- Focus mode
-paq 'junegunn/goyo.vim'
-paq 'junegunn/limelight.vim'
+    -- DevOps Stuff
+    use 'hashivim/vim-terraform'
+    use 'hashicorp/terraform-ls'
 
--- DevOps Stuff
-paq 'hashivim/vim-terraform'
-paq 'hashicorp/terraform-ls'
-
--- Linting error support for js,ts
---  paq 'desmap/ale-sensible'
---  paq 'w0rp/ale', { 'for': ['typescript', 'javascript'] }
+    -- Linting error support for js,ts
+    --  use 'desmap/ale-sensible'
+    --  use 'w0rp/ale', { 'for': ['typescript', 'javascript'] }
+  end
+}
