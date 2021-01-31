38c39f6775393d037bc65c1fc304bbe2462b70fd
Koen Verburg <creativekoen@gmail.com>
2021-01-31 10:09:28 +0100

    improv(nvim) updated the keybindings

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -10,38 +10,32 @@ vim.g.completion_trigger_character = '.'
 
 local nvim_lsp = require('lspconfig')
 
-local on_attach = function(client, bufnr)
-  require('completion').on_attach(client, bufnr)
-  local opts = { noremap=true, silent=true }
+local mapper = function(mode, key, command)
+  vim.api.nvim_buf_set_keymap(0, mode, key, command, { noremap=true, silent=true })
+end
 
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD',  '<Cmd>lua vim.lsp.buf.declaration()<CR>'     , opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd',  '<Cmd>lua vim.lsp.buf.definition()<CR>'      , opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi',  '<cmd>lua vim.lsp.buf.implementation()<CR>'  , opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr',  '<cmd>lua vim.lsp.buf.references()<CR>'      , opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gx',  '<cmd>lua vim.lsp.buf.code_action()<CR>'     , opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gds', '<cmd>lua vim.lsp.buf.document_symbol()<CR>' , opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gws', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', opts)
+local on_attach = function(client)
+  require('completion').on_attach(client)
 
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>sh', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  mapper('n', 'gD',  '<Cmd>lua vim.lsp.buf.declaration()<CR>')
+  mapper('n', 'gd',  '<Cmd>lua vim.lsp.buf.definition()<CR>')
+  mapper('n', '<c-]>',  '<Cmd>lua vim.lsp.buf.definition()<CR>')
 
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>xr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>xd', '<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>', opts)
-  --buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  --buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  --buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  --buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  --buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  --buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  --buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  --buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  --buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  --buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  --buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  --buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  --buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  --buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  --buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  mapper('n', 'gi',  '<cmd>lua vim.lsp.buf.implementation()<CR>')
+  mapper('n', 'gr',  '<cmd>lua vim.lsp.buf.references()<CR>')
+  mapper('n', 'gx',  '<cmd>lua vim.lsp.buf.code_action()<CR>')
+
+  mapper('n', 'gds', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+  mapper('n', 'gW', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+
+  mapper('n', 'H', '<Cmd>lua vim.lsp.buf.hover()<CR>')
+  mapper('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>')
+
+  mapper('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+  mapper('n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+
+  mapper('n', '<leader>rd', '<cmd>lua vim.lsp.buf.rename()<CR>')
+  mapper('n', '<leader>sd', '<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
 end
 
 -- The langauges servers
