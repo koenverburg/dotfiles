eb5475d5d363063369963ee76a90a05ba52b2843
Koen Verburg <creativekoen@gmail.com>
2021-02-10 21:24:45 +0100

    feat(ansible): added ansible galaxy

--- ansible/ubuntu.sh
+++ ansible/ubuntu.sh
@@ -2,5 +2,5 @@
 set -e
 
 export ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-
+ansible-galaxy install -r ./provision/requirements.yml
 ansible-playbook -i ./ansible/inventory/locals ./ansible/playbooks/dotfiles.yml --ask-become-pass;
