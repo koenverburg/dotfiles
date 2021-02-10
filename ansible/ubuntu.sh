692c7fc036dcbd3e32e1773f9382db7025b857d2
Koen Verburg <creativekoen@gmail.com>
2021-02-10 18:43:42 +0100

    refactor: removed host and playbook variables

--- ansible/ubuntu.sh
+++ ansible/ubuntu.sh
@@ -1,22 +1,6 @@
-# /usr/bin/bash
+#!/usr/bin/bash
+set -e
 
-# set -e
-
-##################
-# Global variables
-
-# Dotfiles' project root directory
 export ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-# Host file location
-export HOSTS="$ROOTDIR/ansible/inventory/locals"
-
-# Main playbook
-export PLAYBOOK="$ROOTDIR/ansible/playbooks/dotfiles.yml"
-
-# Installs ansible
-# sudo apt-get update && sudo apt-get install -y ansible
-
-# Runs Ansible playbook using our user.
-ansible-playbook -i "$HOSTS" "$PLAYBOOK" --ask-become-pass --verbose;
-exit 0;
+ansible-playbook -i ./ansible/inventory/locals ./ansible/playbooks/dotfiles.yml --ask-become-pass;
