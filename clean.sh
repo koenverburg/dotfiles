32d46cdd6bf888e930f04f772131822cecf97a9c
Koen Verburg <creativekoen@gmail.com>
2021-03-07 19:53:43 +0100

    chore: added clean up script

--- clean.sh
+++ clean.sh
@@ -1,3 +1,8 @@
-# docker stop $(docker ps -a -q)
-# docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+#!/usr/bin/bash
+
+docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+
+docker images
+docker containers ls -a
