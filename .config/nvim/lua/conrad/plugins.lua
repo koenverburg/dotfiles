60925cc3e4e91c36e87b14031366592d6c740921
Koen Verburg <creativekoen@gmail.com>
2021-05-23 12:01:10 +0200

    refactor(nvim) moved root np files

---
+++ .config/nvim/lua/conrad/plugins.lua
@@ -0,0 +1,102 @@
+-- Only required if you have packer in your `opt` pack
+local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
+
+if not packer_exists then
+  if vim.fn.input("Download Packer? (y for yes)") ~= "y" then
+    return
+  end
+
+  local directory = string.format(
+    '%s/site/pack/packer/opt/',
+    vim.fn.stdpath('data')
+  )
+
+  vim.fn.mkdir(directory, 'p')
+
+  local out = vim.fn.system(string.format(
+    'git clone %s "%s"',
+    'https://github.com/wbthomason/packer.nvim',
+    directory .. '/packer.nvim'
+  ))
+
+  print(out)
+  print("Downloading packer.nvim...")
+  print("( You'll need to restart now )")
+
+  return
+end
+
+return require('packer').startup {
+  function(use)
+    -- use 'tweekmonster/startuptime.vim'
+    use {'wbthomason/packer.nvim', opt = true}
+
+    -- Colorscheme
+    use 'glepnir/zephyr-nvim'
+    use {'maaslalani/nordbuddy', 'tjdevries/colorbuddy.nvim'}
+
+    -- Finders / UI
+    use 'nvim-lua/popup.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use 'nvim-telescope/telescope.nvim'
+    use 'nvim-telescope/telescope-fzy-native.nvim'
+    use 'nvim-telescope/telescope-fzf-writer.nvim'
+    use 'nvim-treesitter/nvim-treesitter-textobjects'
+    use 'JoosepAlviste/nvim-ts-context-commentstring'
+    use 'scrooloose/nerdtree'
+
+    -- LSP config
+    use 'neovim/nvim-lspconfig'
+    use 'nvim-lua/completion-nvim'
+    -- use 'nvim-lua/diagnostic-nvim' -- this is deprecated
+    use 'onsails/lspkind-nvim'
+    use 'glepnir/lspsaga.nvim'
+    --use 'tjdevries/nlua.nvim'
+
+    -- Snippets
+    --use 'SirVer/ultisnips'
+
+    -- Utils
+    use 'glepnir/prodoc.nvim'
+    use 'nvim-treesitter/nvim-treesitter'
+
+    -- Misc
+    use 'tpope/vim-surround' -- Able to change [{()}]""''
+    use 'tpope/vim-fugitive'
+    use 'tpope/vim-commentary'
+    use 'RRethy/vim-illuminate'
+    use 'jiangmiao/auto-pairs'
+    -- use 'tjdevries/astronauta.nvim'
+
+    -- Lanaguages
+    use 'elzr/vim-json'
+    use 'stephpy/vim-yaml'
+    use 'PProvost/vim-ps1'
+    use 'hashivim/vim-terraform'
+    use 'hashicorp/terraform-ls'
+    use 'ekalinin/Dockerfile.vim'
+    use 'jidn/vim-dbml'
+
+    --  Markdown
+    use 'plasticboy/vim-markdown'
+    use 'godlygeek/tabular'
+
+    -- use 'bkad/CamelCaseMotion' -- WordJumping like resharper and faster movement
+
+    -- Looks
+    use 'mhinz/vim-startify'
+    use 'tjdevries/express_line.nvim'
+    use 'lewis6991/gitsigns.nvim'
+
+    -- icons
+    use 'kyazdani42/nvim-web-devicons'
+    use 'ryanoasis/vim-devicons'
+
+    -- Focus mode
+    use 'junegunn/goyo.vim'
+    use 'junegunn/limelight.vim'
+
+    -- linting
+    use 'w0rp/ale'
+  end
+}
