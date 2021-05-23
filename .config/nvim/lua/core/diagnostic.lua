0184f89bc337efcc80d23568ad763a511d241ad7
Koen Verburg <creativekoen@gmail.com>
2021-05-23 12:00:32 +0200

    refactor(nvim) clean up and moving to namespace (conrad)

--- .config/nvim/lua/core/diagnostic.lua
+++
@@ -1,7 +0,0 @@
---vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  --vim.lsp.diagnostic.on_publish_diagnostics, {
-    --signs = true,
-    --underline = true,
-    --update_in_insert = false
-  --}
---)
