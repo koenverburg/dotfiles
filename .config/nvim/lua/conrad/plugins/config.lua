cb419e283689f47188c417959406d15a9b5f1231
Koen Verburg <creativekoen@gmail.com>
2021-06-15 17:22:06 +0200

    program(nvim) remove ale config

--- .config/nvim/lua/conrad/plugins/config.lua
+++ .config/nvim/lua/conrad/plugins/config.lua
@@ -5,17 +5,6 @@ vim.cmd [[ augroup END ]]
 
 vim.g.DevIconsAppendArtifactFix = 1
 
-local linterAndFixers = {'eslint', 'gofmt', 'golint', 'terraform', 'terraform-ls', 'ansible-lint', 'stylelint', 'spectral'}
-vim.b.ale_fixers = linterAndFixers
-vim.b.ale_linters = linterAndFixers
-
-vim.g.ale_sign_column_always = 1
-vim.g.ale_echo_msg_error_str = 'E'
-vim.g.ale_echo_msg_warning_str = 'W'
-vim.g.ale_echo_msg_format = '[%linter%] %s [%severity%]'
-vim.g.ale_sign_error = '●'
-vim.g.ale_sign_warning = '.'
-
 -- focus mode
 vim.cmd [[ autocmd! User GoyoEnter Limelight ]]
 vim.cmd [[ autocmd! User GoyoLeave Limelight! ]]
