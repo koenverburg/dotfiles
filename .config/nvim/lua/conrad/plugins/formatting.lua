e555c55345a0b6ef11ca88a618f329fe27e8236c
Koen Verburg <creativekoen@gmail.com>
2021-06-27 21:45:34 +0200

    program(nvim) abstracted eslint formatting for js/ts/jsreact/tsreact

--- .config/nvim/lua/conrad/plugins/formatting.lua
+++ .config/nvim/lua/conrad/plugins/formatting.lua
@@ -1,26 +1,23 @@
+local eslint = {
+  -- eslint
+  function()
+    return {
+      exe = 'eslint_d',
+      args = {'--stdin', '--fix-to-stdout', '--stdin-filename', vim.api.nvim_buf_get_name(0) },
+      stdin = true
+    }
+  end
+}
+
 require('formatter').setup({
   logging = false,
   filetype = {
-    typescript = {
-      -- eslint
-      function()
-        return {
-          exe = 'eslint_d',
-          args = {'--stdin', '--fix-to-stdout', '--stdin-filename', vim.api.nvim_buf_get_name(0) },
-          stdin = true
-        }
-      end
-    },
-    javascript = {
-      -- eslint
-      function()
-        return {
-          exe = 'eslint_d',
-          args = {'--stdin', '--fix-to-stdout', '--stdin-filename', vim.api.nvim_buf_get_name(0) },
-          stdin = true
-        }
-      end
-    },
+
+    typescript = eslint,
+    typescriptreact = eslint,
+
+    javascript = eslint,
+    javascriptreact = eslint,
     -- lua = {
     --     function()
     --       return {
