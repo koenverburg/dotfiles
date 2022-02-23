a6d85e82b11d2b30b543c048f3297b121ec42d23
Koen Verburg <creativekoen@gmail.com>
2022-02-23 10:06:06 +0100

    chore: random stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,22 +2,21 @@
 -- Author: Koen Verburg <creativekoen@gmail.com>
 -- Source: https://github.com/koenverburg/dotfiles
 
-require('impatient')
-require('packer_compiled')
+require("impatient")
+require("packer_compiled")
 
-require('conrad.disable_builtin')
-require('conrad.plugins')
-require('conrad.globals')
-require('conrad.binds')
+require("conrad.disable_builtin")
+require("conrad.plugins")
+require("conrad.globals")
+require("conrad.binds")
 
-require('conrad.telescope')
-require('conrad.lsp')
+require("conrad.telescope")
+require("conrad.lsp")
 
-require('conrad.core.treesitter')
-require('conrad.core.statusline')
+require("conrad.core.treesitter")
+require("conrad.core.statusline")
 
-require('conrad.plugins.startup')
-require('conrad.plugins.config')
-require('conrad.plugins.gitsigns')
-require('conrad.plugins.formatting')
-
+require("conrad.plugins.startup")
+require("conrad.plugins.config")
+require("conrad.plugins.gitsigns")
+require("conrad.plugins.formatting")
