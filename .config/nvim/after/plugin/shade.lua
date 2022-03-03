187da82c7aaee769013273952a9f4ae1d892fceb
Koen Verburg <creativekoen@gmail.com>
2022-03-03 20:13:59 +0100

    feat(nvim) added shade plugin

---
+++ .config/nvim/after/plugin/shade.lua
@@ -0,0 +1,10 @@
+require("shade").setup {
+  opacity_step = 10,
+  overlay_opacity = 30,
+
+  keys = {
+    toggle = "<Leader>s",
+    brightness_up = "<C-Up>",
+    brightness_down = "<C-Down>",
+  }
+}
--- .config/nvim/lua/conrad/packer.lua
+++ .config/nvim/lua/conrad/packer.lua
@@ -64,6 +64,7 @@ return require("packer").startup {
     use "ThePrimeagen/harpoon"
     use "jose-elias-alvarez/null-ls.nvim"
     use "numToStr/Comment.nvim"
+    use 'sunjon/shade.nvim'
     -- use 'glepnir/prodoc.nvim'
     -- use 'bkad/CamelCaseMotion' -- WordJumping like resharper and faster movement
 
