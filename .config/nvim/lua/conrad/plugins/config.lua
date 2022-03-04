c7eaaa04901f56a1f9a5634be64e4579dcb2c51c
Koen Verburg <creativekoen@gmail.com>
2022-03-04 16:35:26 +0100

    feat(nvim) added autopairs, its very nice!

--- .config/nvim/lua/conrad/plugins/config.lua
+++ .config/nvim/lua/conrad/plugins/config.lua
@@ -1,6 +1,14 @@
-require("Comment").setup() -- The bindings need to updated from this plugin
+require("fidget").setup()
 require("nvim-tree").setup()
 require("session-lens").setup()
 require("minimal-tabline").setup()
-require("surround").setup { mappings_style = "surround" } -- Does this one even work?
-require("fidget").setup()
+
+
+require("Comment").setup() -- The bindings need to updated from this plugin
+
+require('nvim-autopairs').setup()
+
+local Rule = require('nvim-autopairs.rule')
+local npairs = require('nvim-autopairs')
+
+npairs.add_rule(Rule("<",">"))
