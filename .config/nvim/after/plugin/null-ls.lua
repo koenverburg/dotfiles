6af06e47e8c67e57543afbc5304f0937eaae949c
Koen Verburg <creativekoen@gmail.com>
2022-03-07 18:57:37 +0100

    program(nvim) added os check to insert eslint or standardjs for formatting

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -7,13 +7,29 @@ local spell = ls.builtins.completion
 local formatting = ls.builtins.formatting
 local diagnostics = ls.builtins.diagnostics
 
+local os = vim.loop.os_uname().sysname
+
+local core_sources = {
+  -- completion.spell,
+  formatting.gofmt,
+  formatting.stylua, -- install with cargo install stylua
+  formatting.prettier,
+}
+
+local function TableConcat(t1, t2)
+  for i = 1, #t2 do
+    t1[#t1 + 1] = t2[i]
+  end
+  return t1
+end
+
+local sources = core_sources
+if os == "Darwin" then
+  sources = TableConcat(sources, { formatting.eslint })
+else
+  sources = TableConcat(sources, { formatting.standardjs })
+end
+
 ls.setup {
-  sources = {
-    -- completion.spell,
-    -- formatting.eslint,
-    formatting.gofmt,
-    formatting.stylua, -- install with cargo install stylua
-    formatting.prettier,
-    formatting.standardjs,
-  },
+  sources = sources,
 }
