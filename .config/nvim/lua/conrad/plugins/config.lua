516aff52805f7e8c8acac94727b311af44e3f1f2
Koen Verburg <creativekoen@gmail.com>
2022-03-08 21:57:48 +0100

    chore(nvim) changed whitespaces?

--- .config/nvim/lua/conrad/plugins/config.lua
+++ .config/nvim/lua/conrad/plugins/config.lua
@@ -3,12 +3,10 @@ require("nvim-tree").setup()
 require("session-lens").setup()
 require("minimal-tabline").setup()
 
-
 require("Comment").setup() -- The bindings need to updated from this plugin
 
 require('nvim-autopairs').setup()
-
 local Rule = require('nvim-autopairs.rule')
 local npairs = require('nvim-autopairs')
-
 npairs.add_rule(Rule("<",">"))
+
