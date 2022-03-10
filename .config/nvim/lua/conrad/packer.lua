bede5225ad0e770b1e567173d9d6fa3fbfa17bd7
Koen Verburg <creativekoen@gmail.com>
2022-03-10 22:00:48 +0100

    program(nvim) added dim plugin

--- .config/nvim/lua/conrad/packer.lua
+++ .config/nvim/lua/conrad/packer.lua
@@ -21,6 +21,7 @@ return require("packer").startup {
     use "nvim-treesitter/nvim-treesitter"
     use "nvim-treesitter/nvim-treesitter-textobjects"
     use "JoosepAlviste/nvim-ts-context-commentstring"
+    use 'narutoxy/dim.lua'
 
     -- LSP config
     use "neovim/nvim-lspconfig"
--- .config/nvim/lua/conrad/plugins/config.lua
+++ .config/nvim/lua/conrad/plugins/config.lua
@@ -2,11 +2,11 @@ require("fidget").setup()
 require("nvim-tree").setup()
 require("session-lens").setup()
 require("minimal-tabline").setup()
+require("dim").setup()
 
 require("Comment").setup() -- The bindings need to updated from this plugin
 
-require('nvim-autopairs').setup()
-local Rule = require('nvim-autopairs.rule')
-local npairs = require('nvim-autopairs')
-npairs.add_rule(Rule("<",">"))
-
+require("nvim-autopairs").setup()
+local Rule = require "nvim-autopairs.rule"
+local npairs = require "nvim-autopairs"
+npairs.add_rule(Rule("<", ">"))
