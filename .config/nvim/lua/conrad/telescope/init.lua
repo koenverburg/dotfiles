75ea9974a92ec28f24693c594cc33d17fb524cbb
Koen Verburg <creativekoen@gmail.com>
2022-03-28 22:22:33 +0200

    program(nvim) updated find_files layout

--- .config/nvim/lua/conrad/telescope/init.lua
+++ .config/nvim/lua/conrad/telescope/init.lua
@@ -9,7 +9,7 @@ require("telescope").setup {
   },
   extensions = {
     fzf = {
-      fuzzy = false, -- false will only do exact matching
+      fuzzy = true, -- false will only do exact matching
       override_file_sorter = true, -- override the file sorter
       override_generic_sorter = true, -- override the generic sorter
       case_mode = "ignore_case", -- or "ignore_case" or "respect_case"
@@ -106,7 +106,7 @@ end
 
 function M.find_files()
   local opts = {
-    layout_strategy = "vertical",
+    layout_strategy = "horizontal",
     layout_config = {
       prompt_position = "bottom",
     },
