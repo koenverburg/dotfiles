3a1311a3915bed65a69d9509d47e6511dd15c374
Koen Verburg <creativekoen@gmail.com>
2022-04-12 12:59:05 +0200

    chore(nvim) turned of fuzzy search

--- .config/nvim/lua/conrad/telescope/init.lua
+++ .config/nvim/lua/conrad/telescope/init.lua
@@ -10,7 +10,7 @@ require("telescope").setup {
   },
   extensions = {
     fzf = {
-      fuzzy = true, -- false will only do exact matching
+      fuzzy = false, -- false will only do exact matching
       override_file_sorter = true, -- override the file sorter
       override_generic_sorter = true, -- override the generic sorter
       case_mode = "ignore_case", -- or "ignore_case" or "respect_case", the default case_mode is "smart_case"
