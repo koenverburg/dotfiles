760b9fc42983c69aa8317519d80337524c898389
Koen Verburg <creativekoen@gmail.com>
2022-07-04 10:35:23 +0200

    chore(tmux) updated battery script

--- .config/tmux/battery.sh
+++ .config/tmux/battery.sh
@@ -4,12 +4,12 @@ battery() {
   local batt discharging percentage charge
 
   if [[ $(uname) == "Linux" ]]; then
-    batt0=/sys/class/power_supply/BAT0
+    # batt0=/sys/class/power_supply/BAT0
     batt=$(acpi -b 2> /dev/null | awk '/Battery 0/{print $4}' | cut -d, -f1)
     # discharging=$(grep -qi "discharging" ${batt}/status && echo "true" || echo "false")
   elif [[ $(uname) == "Darwin" ]]; then
-    batt="$(pmset -g batt)"
-    # discharging="$(echo "${batt}" | grep -qi "discharging" && echo "true" || echo "false")"
+    batt0="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
+    batt="$batt0%"
   else
     return 1
   fi
