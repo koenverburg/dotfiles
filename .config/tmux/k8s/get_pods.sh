f975b99caf5754826cc2daee281c1a4ba10ccb61
Koen Verburg <creativekoen@gmail.com>
2022-12-30 11:07:38 +0100

    chore(tmux) removed k8s bash scripts

--- .config/tmux/k8s/get_pods.sh
+++
@@ -1,6 +0,0 @@
-get_running_pods() {
-  pods=$(kubectl get pods --no-headers --field-selector=status.phase=Running --request-timeout 1s | wc -l | awk '{ print $1 }')
-  echo "p:#[bold]$pods#[nobold]"
-}
-
-get_running_pods
