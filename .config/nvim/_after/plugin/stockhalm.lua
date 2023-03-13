8b672d85958b1ff9981df08823ab383b0f576b3c
Koen Verburg <creativekoen@gmail.com>
2023-03-13 21:55:53 +0100

    chore(nvim) underscrored after folder

---
+++ .config/nvim/_after/plugin/stockhalm.lua
@@ -0,0 +1,36 @@
+local StockhalmDark = {}
+-- TODO creat to others Sepia and light
+
+--Light colors
+StockhalmDark.background  = "#0b1015"
+StockhalmDark.Red         = "#FB467B"
+StockhalmDark.Blue        = "#0BA8E2"
+StockhalmDark.Green       = "#B8EE92"
+StockhalmDark.Purple      = "#975EEC"
+StockhalmDark.Yellow      = "#FFCC00"
+StockhalmDark.Orange      = "#ff8d03"
+StockhalmDark.Violet      = "#F281F2"
+StockhalmDark.Magenta     = "#F95CE6"
+StockhalmDark.Pink        = "#DB73DA"
+StockhalmDark.White       = "#CED5E5"
+StockhalmDark.Cyan        = "#80a0ff"
+StockhalmDark.Aqua        = "#00D5A7"
+StockhalmDark.Black       = "#0b1015"
+StockhalmDark.Grey        = "#494646"
+StockhalmDark.LightGrey   = "#13191F"
+StockhalmDark.Custom_1    = "#2D3036"
+StockhalmDark.Custom_2    = "#AFFDF1"
+StockhalmDark.Custom_3    = "#E2E7E6"
+
+--Dark colors
+StockhalmDark.DarkRed     = "#FD2E6A"
+StockhalmDark.DarkOrange  = "#ff8d03"
+StockhalmDark.DarkBlue    = "#007ED3"
+StockhalmDark.DarkGreen   = "#5EB95D"
+StockhalmDark.DarkYellow  = "#FFCC00"
+StockhalmDark.DarkMagenta = "#FE92E1"
+StockhalmDark.DarkCyan    = "#56D6D6"
+StockhalmDark.DarkGrey    = "#555555"
+StockhalmDark.DarkGrey_2  = "#828989"
+
+return StockhalmDark
