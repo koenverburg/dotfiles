9ccbc8627b5be1457ced8f00c4a10b45f80f0382
Koen Verburg <creativekoen@gmail.com>
2023-04-17 07:54:53 +0200

    saving point, prep new work laptop

---
+++ .config/nvim/colors/nightbuddy.lua
@@ -0,0 +1,26 @@
+local Color, colors, Group, groups, styles = require('colorbuddy').setup()
+
+Color.new('white',     '#f2e5bc')
+Color.new('red',       '#cc6666')
+Color.new('pink',      '#fef601')
+Color.new('green',     '#99cc99')
+Color.new('yellow',    '#f8fe7a')
+Color.new('blue',      '#81a2be')
+Color.new('aqua',      '#8ec07c')
+Color.new('cyan',      '#8abeb7')
+Color.new('purple',    '#8e6fbd')
+Color.new('violet',    '#b294bb')
+Color.new('orange',    '#de935f')
+Color.new('brown',     '#a3685a')
+
+Color.new('seagreen',  '#698b69')
+Color.new('turquoise', '#698b69')
+
+-- Color.new('background','#090807')
+
+-- Group.new('Info', colors.background, nil, nil)
+-- Color.new('background',	'#21272e')
+-- Color.new('black',     	'#21272e')
+-- Group.new('CursorLine', nil, colors.background, nil)
+-- Group.new('CursorColumn', groups.CursorLine, groups.CursorLine, groups.CursorLine)
+
--- streaming/commit/main.go
+++ streaming/commit/main.go
@@ -3,10 +3,31 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"regexp"
 
-	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5"
+	"github.com/pkg/errors"
 )
+
+func findTopLevelGitDir(workingDir string) (string, error) {
+	dir, err := filepath.Abs(workingDir)
+	if err != nil {
+		return "", errors.Wrap(err, "invalid working dir")
+	}
+
+	for {
+		if _, err := os.Stat(filepath.Join(dir, ".git")); err == nil {
+			return dir, nil
+		}
+
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			return "", errors.New("no git repository found")
+		}
+		dir = parent
+	}
+}
 
 func getBranch() string {
 	path, err := os.Getwd()
@@ -14,33 +35,42 @@ func getBranch() string {
 		fmt.Println(err)
 	}
 
-  repo, err := git.PlainOpen(path)
+  topLevel, err := findTopLevelGitDir(path)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-  h, err := repo.Head()
+	repo, err := git.PlainOpen(topLevel)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-  return h.Name().Short()
+	h, err := repo.Head()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return h.Name().Short()
 }
 
 func getTicket(branch string) string {
-  var re = regexp.MustCompile(`(?m)(\w+-\d+)`)
+	re := regexp.MustCompile(`(?m)(\w+-\d+)`)
 
-  matches := re.FindAllString(branch, -1)
+	matches := re.FindAllString(branch, -1)
 
-  if len(matches) > 0 {
-    return matches[0]
-  }
+	if len(matches) > 0 {
+		return matches[0]
+	}
 
-  return ""
+	return ""
 }
 
 func main() {
-  branch := getBranch()
-  ticket := getTicket(branch)
-  fmt.Println(ticket)
+	branch := getBranch()
+
+  fmt.Println(branch)
+
+	// ticket := getTicket(branch)
+
+	// fmt.Println(ticket)
 }
